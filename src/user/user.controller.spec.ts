b359c8f0323b45a647e7a93ae1376159108d4d0b
dojoohwan <ehwnghks@gmail.com>
2023-08-31 22:38:37 +0900

    user 단위테스트 test

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,18 +1,21 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [UserController],
+      providers: [UserService],
+      controllers: [UserController]
     }).compile();
 
     controller = module.get<UserController>(UserController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('getHelloWorld() 의 실행 결과 테스트', () => {
+    const result = controller.getHelloWorld();
+    expect(result).toEqual('Hello World!!');
   });
 });
