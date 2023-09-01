108e366d12b415ec91c9d296a0cb7ffd51bc1494
dojoohwan <ehwnghks@gmail.com>
2023-09-01 18:23:21 +0900

    환경변수 관리를 위한 typeorm config 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeormConfig } from './config/typeorm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env.${process.env.NODE_ENV}'
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: TypeormConfig
     }),
     UserModule
   ],
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,27 @@
+import { ConfigService } from "@nestjs/config";
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+export function TypeormConfig(configService: ConfigService){
+    const env = configService.get('NODE_ENV');
+    if(!['dev', 'prod'].includes(env)) {
+        throw Error('dev나 prod 환경이어야 합니다.');
+    }
+
+    const synchronize = configService.get<string>('SYNCHRONIZE') === 'true' ? true : false;
+    const logging = configService.get<string>('DB_LOGGING') === 'true' ? true : false;
+    const DB_TYPE: 'mysql' | null = 'mysql';
+
+    const option: TypeOrmModuleOptions = {
+        type: DB_TYPE,
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: env === 'prod' ? false : synchronize,
+        logging: logging
+    };
+
+    return option;
+}
